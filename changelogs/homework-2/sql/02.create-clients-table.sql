2545d8250497382c8a705707f1d36853bb0a88a7
Daniil Savko <135634324+zzoddiaak@users.noreply.github.com>
2024-03-22 15:11:11 +0300

    Update 02.create-clients-table.sql

--- changelogs/homework-2/sql/02.create-clients-table.sql
+++ changelogs/homework-2/sql/02.create-clients-table.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS public.clients
     id serial PRIMARY KEY,
     user_id integer UNIQUE,
     client_card_id integer UNIQUE,
-    CONSTRAINT "user" FOREIGN KEY (id) REFERENCES public.users (id),
     CONSTRAINT client FOREIGN KEY (user_id) REFERENCES public.users (id),
     CONSTRAINT card FOREIGN KEY (client_card_id) REFERENCES public.clients_cards (id)
 );

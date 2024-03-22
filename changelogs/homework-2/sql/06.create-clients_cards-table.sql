d96e18e27498b92569107f54209e6503de54970b
Daniil Savko <135634324+zzoddiaak@users.noreply.github.com>
2024-03-22 14:59:23 +0300

    Update 06.create-clients_cards-table.sql

--- changelogs/homework-2/sql/06.create-clients_cards-table.sql
+++ changelogs/homework-2/sql/06.create-clients_cards-table.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE IF NOT EXISTS public.clients_cards
 (
-    id integer PRIMARY KEY,
+    id serial PRIMARY KEY,
     psychologist_id integer,
     diagnos character varying(255),
     recommendations text,

01d668568a96f2ef865f77a3b973dc834fdc6798
Daniil Savko <135634324+zzoddiaak@users.noreply.github.com>
2024-04-06 22:10:03 +0300

    Delete src/main/java/senla/repository directory

--- src/main/java/senla/repository/implement/RolePermissionRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package senla.repository.implement;
-
-import org.springframework.stereotype.Repository;
-import senla.repository.AbstractRepository;
-@Repository
-
-public class RolePermissionRepositoryImpl extends AbstractRepository {
-}

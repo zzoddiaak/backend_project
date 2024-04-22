b1a7adbfa608e6895549e1a00ed5ed09aae163fe
Daniil Savko <135634324+zzoddiaak@users.noreply.github.com>
2024-04-23 00:25:12 +0300

    Update SessionServiceImpl.java

--- src/main/java/senla/servise/implement/SessionServiceImpl.java
+++ src/main/java/senla/servise/implement/SessionServiceImpl.java
@@ -2,6 +2,7 @@ package senla.servise.implement;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import senla.mapper.SessionMapper;
 import senla.dto.session.SessionDTOToEntity;
 import senla.dto.session.SessionShortDTO;
@@ -16,23 +17,26 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SessionServiceImpl implements SessionService {
     private final SessionRepository sessionRepository;
-
+    @Transactional
     @Override
     public List<SessionShortDTO> findAll() {
         return sessionRepository.findAll().stream()
                 .map(SessionMapper::convertToShortDto)
                 .toList();
     }
+    @Transactional
     @Override
     public SessionShortDTO findById(long uuid) {
         Session session = sessionRepository.findById(uuid);
         return session != null ? SessionMapper.convertToShortDto(session) : null;
     }
+    @Transactional
     @Override
     public boolean save(SessionDTOToEntity object) {
         sessionRepository.save(SessionMapper.createSessionDto(object));
         return true;
     }
+    @Transactional
     @Override
     public boolean update(long uuid, SessionDTOToEntity updateDTO) {
         Session session = sessionRepository.findById(uuid);
@@ -48,7 +52,7 @@ public class SessionServiceImpl implements SessionService {
         }
         return false;
     }
-
+    @Transactional
     @Override
     public void deleteById(long uuid) {
         sessionRepository.deleteById(uuid);
